5201cd57f001f05edbc8517188ae3a254f10b8ea
Aniketh <aaatipamula@gmail.com>
2023-02-05 21:20:47 -0600

    added components and island for recent projects

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Aniketh A</title>
       </Head>
-      <div class="w-screen h-screen p-3 bg-[#0D0D0D]">
+      <div class="w-screen h-screen p-3 m-0 bg-[#0D0D0D]">
         <Navbar></Navbar>
-        <div class="flex items-center justify-center px-6 py-20 mx-auto my-3 w-2/3 rounded-xl font-medium font-sans text-6xl text-[#404040] bg-[#A68F8A]">Hi, I'm Aniketh</div>
+        <div class="flex items-center justify-center px-6 py-20 mx-auto my-3 w-2/3 font-medium font-sans text-6xl text-[#A68F8A]">Hi, I'm Aniketh</div>
         <h2 class="">Recent Projects</h2>
         <div>
           <h2>Music I Like</h2>
